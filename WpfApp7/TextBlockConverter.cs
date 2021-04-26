403e6133517cd2393a2fd6af81bbb0ea69600237
דניאל לוי <levidaniel140@gmail.com>
2021-04-26 14:34:06 +0300

    Add project files.

---
+++ WpfApp7/TextBlockConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp7
+{
+    public class TextBlockConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value >= 90 ? Brushes.Red : Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
